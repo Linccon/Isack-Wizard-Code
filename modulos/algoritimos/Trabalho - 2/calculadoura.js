bb26fba55965a8452a48fd8d0bf762bdf55b6c6e
Linccon <isacklinccon@gmail.com>
2021-02-05 01:14:19 -0300

    calculadoura.js

---
+++ modulos/algoritimos/Trabalho - 2/calculadoura.js
@@ -0,0 +1,75 @@
+// Soma
+const somaA = 8;
+const somaB = 5;
+
+const soma = somaA + somaB;
+
+console.log("soma é de ${somaA} com ${somaB} é igual a ${soma}");
+
+console.log(soma);
+
+// Subtração
+
+const subtraçãoA = 7;
+const subtraçãoB = 3;
+
+const subtração = subtraçãoA - subtraçãoB;
+
+console.log("subtração de ${subtraçãoA} com ${subtraçãoB} é igual a subtração");
+
+console.log(subtração);
+
+//  Multiplicação
+
+const multiplicaçãoA = 10;
+const multiplicaçãoB = 5;
+
+const multiplicação = multiplicaçãoA * multiplicaçãoB;
+
+console.log(
+  "a Multiplicação de ${multiplicaçãoA} com ${multiplicaçãoB} é igual a multiplicação"
+);
+
+console.log(multiplicação);
+
+// divisão
+
+const divisãoA = 60;
+const divisãoB = 20;
+
+const divisão = divisãoA / divisãoB;
+
+console.log("a divisão de ${divisãoA} com ${divisãoB} é igual a divisão");
+
+// Raiz e Exponeciação
+
+const numero = 122;
+const expoente = 8;
+
+const raiz = Math.sqrt(122);
+
+console.log("raiz é igual a ${Math.sqrt(numero)}");
+
+console.log(raiz);
+
+const exponeciação = Math.pow(122, 8);
+
+console.log("exponeciação é ${Math.pow(numero,expoente");
+
+console.log(exponeciação);
+
+//  Baskhara
+
+const a = 4,
+  b = 5,
+  c = -7;
+
+const delta = b * b - 4 * a * c;
+
+const x1 = (-b + Math.sqrt(delta)) / (2 * a);  
+const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+console.log("A soma de x1 e de x2 para ${a}a, ${b}b , ${c}c é ${resultado}");
+ 
+const resultado = x1 + x2;
+
